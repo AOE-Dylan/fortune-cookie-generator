5c7c3dd76094feee8dc7eb54562bba36f5b5ea7a
AOE-Dylan <dylantt2000@gmail.com>
2017-01-11 09:22:40 -1000

    stuff

--- js/app.js
+++ js/app.js
@@ -2,24 +2,17 @@ console.log("Hello, how are you?");
 
 function generateFortuneCookie() {
   document.getElementById('fortune-cookie-text').style.display = "block";
-  var fortunes = {
-    begin : ["You will meet a ", "You might run into a ", "Out of nowhere, a "],
-    people : ["handsome superhero ", "billionaire", "famous movie star"],
-    action : ["will deliver an important message", " will need your help", "will want to give you special stuff"]
-  };
-  for (var i = 0; i < 3; i++) {
-  var printFortune = document.getElementById('read').innerHTML =   
-    (fortunes.begin[i] + fortunes.people[i] + " " + fortunes.action[i] + " !");
-    document.getElementById('read').style.display = "block";
-  previousFortunes(printFortune)
-  }
+  var begin = ["You will meet a ", "You might run into a ", "Out of nowhere, a "];
+  var people = ["handsome superhero ", "billionaire", "famous movie star"];
+  var action = ["will deliver an important message", " will need your help", "will want to give you special stuff"];
+  document.createElement("div#read") = begin[Math.floor(Math.random() * begin.length)] + people[Math.floor(Math.random() * people.length)] + " " + action[Math.floor(Math.random() * action.length)] + " !";
 }
 
 
 
 function previousFortunes(a) {
-    var previousFortunes = document.createElement("previous-fortunes");
     var block = document.createTextNode(a);
-    previousFortunes.appendChild(block);
-    document.getElementById("fortune-cookie-text").appendChild(previousFortunes);
+    pre.appendChild(block);
+    document.getElementById("pre").appendChild(fortunes);
+    document.getElementById('pre').style.display = "block";
 }
